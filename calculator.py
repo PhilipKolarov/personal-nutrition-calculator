e7dc2e698690c8b37047d87c7ab584af49a4fd21
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-03-08 13:49:14 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -8,22 +8,30 @@ total_fat = 0
 total_sat_fat = 0
 food_list = []
 
+def print_total():
+    print(f"{total_calories}kcal | {total_protein}g protein | {total_carbs}g carbs | {total_fat}g fat | {total_sat_fat}g saturated fat")
+
+
 while True:
     current_input = input()
 
     if current_input == "END":
         break
 
-    current_input_array = current_input.split(" X ")
-    qty = float(current_input_array[0])
-    product = current_input_array[1]
-    food_list_adder(food_list, product, qty)
+    if current_input == 'print_total':
+        print_total()
+    else:
+        current_input_array = current_input.split(" X ")
+        qty = float(current_input_array[0])
+        product = current_input_array[1]
+        food_list_adder(food_list, product, qty)
 
-    product_obj = food_library[product]
-    total_calories += qty * product_obj[0]
-    total_protein += qty * product_obj[1]
-    total_carbs += qty * product_obj[2]
-    total_fat += qty * product_obj[3]
-    total_sat_fat += qty * product_obj[4]
+        product_obj = food_library[product]
+        total_calories += qty * product_obj[0]
+        total_protein += qty * product_obj[1]
+        total_carbs += qty * product_obj[2]
+        total_fat += qty * product_obj[3]
+        total_sat_fat += qty * product_obj[4]
 
-print(f"{total_calories}kcal | {total_protein}g protein | {total_carbs}g carbs | {total_fat}g fat | {total_sat_fat}g saturated fat")
+def print_total():
+    print(f"{total_calories}kcal | {total_protein}g protein | {total_carbs}g carbs | {total_fat}g fat | {total_sat_fat}g saturated fat")
