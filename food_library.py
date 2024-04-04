2b83f67b9e517dc896713585756df1697b52258b
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2024-04-04 09:10:42 +0300

    Update food_library.py

--- food_library.py
+++ food_library.py
@@ -10,6 +10,7 @@ food_library = {
     "Fish Finger": [46, 2.9, 3.6, 2.3, 0.5],
     "Greek Yogurt": [200, 12.8, 19.6, 8, 5.2],
     "Milk 100ml": [48, 3.3, 4.7, 1.7, 1.1],
+    "Muesli 87g": [413, 7.9, 54, 16.6, 1.4],
     "Ryvita": [37, 1, 7, 0.1, 0],
     "Brown Toast": [74, 2.3, 14.5, 0.4, 0.1],
     "Protein Bar": [169, 22.5, 12.9, 4.9, 2.8],
@@ -21,6 +22,7 @@ food_library = {
     "Wine 250ml": [203, 0.2, 6.5, 0, 0],
     "Apple": [72, 0.3, 19, 0.2, 0],
     "Carrot 100g": [41, 0.9, 10, 0.2, 0],
+    "Potato 100g": [93, 2.5, 21, 0.1, 0],
     "Little Gem Lettuce": [13, 0.6, 1.4, 0.4, 0.1],
     "Cucumber L": [42, 1.8, 10, 0.3, 0.1],
     "Brocolli 1sv.": [55, 3.7, 11, 0.6, 0.1],
@@ -33,7 +35,6 @@ food_library = {
     "Hunter's Sausage": [140, 6.3, 1.5, 12.3, 5.2],
     "Vanilla Milk": [72, 3.4, 11.1, 1.5, 0.9]
 }
-
 def print_food_library():
     for key, value in food_library.items():
         print(f"{key}: {value[0]}kcal, {value[1]}g protein, {value[2]}g carbs, {value[3]}g fat, {value[4]}g saturated fat")
